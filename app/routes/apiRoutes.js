10f291ff31d300ec26dbcedf51cd2c2915d00816
xingchen19891 <50303394+xingchen19891@users.noreply.github.com>
2019-10-29 21:39:48 -0700

    update file

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -4,8 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+var friendsData = require("../data/friends");
 
 
 // ===============================================================================
@@ -19,13 +18,11 @@ module.exports = function(app) {
   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
   });
 
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
-  });
+  
 
   // API POST Requests
   // Below code handles when a user submits a form and thus submits data to the server.
@@ -35,19 +32,7 @@ module.exports = function(app) {
   // Then the server saves the data to the tableData array)
   // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
-  });
+;
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
